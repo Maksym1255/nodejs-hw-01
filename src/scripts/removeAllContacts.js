bfa10783af11465bbc1de9fd50108fcdfbe67d9e
Maksym Kosenko <maksym.kosenko2014@gmail.com>
2024-09-27 13:41:37 +0300

    add

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,3 +1,13 @@
-export const removeAllContacts = async () => {};
+import fs from 'node:fs/promises';
+import { PATH_DB } from '../constants/contacts.js';
+
+export const removeAllContacts = async () => {
+  try {
+    await fs.writeFile(PATH_DB, JSON.stringify([], null, 2), 'utf-8');
+    console.log('All contacts have been removed.');
+  } catch (error) {
+    console.log('Error removing contacts: ', error.message);
+  }
+};
 
 removeAllContacts();
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,12 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  try {
+    const data = JSON.stringify(updatedContacts, null, 2);
+    await fs.writeFile(PATH_DB, data, 'utf-8');
+    console.log('Contacts successfully added');
+  } catch (error) {
+    console.log('Error writing contacts: ', error.message);
+  }
+};
